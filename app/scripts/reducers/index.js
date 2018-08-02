90414e7581d12e921b7fd87cf7d4fcc2f78b8488
Derek Lieu <dl@dereklieu.com>
2018-08-02 17:02:08 -0400

    Calculate national seat tallies on slider change

--- app/scripts/reducers/index.js
+++ app/scripts/reducers/index.js
@@ -4,18 +4,34 @@ import { feature as toGeojson } from 'topojson';
 const raw = require('../static/tl_2016_us_cd115-quantized-topo.json');
 const districts = toGeojson(raw, raw.objects.districts).features;
 
+const initialNationalVote = 50;
+
 // TODO: remove this and integrate actual data
 districts.forEach(d => {
   d.properties.threshold = 35 + Math.random() * 30;
 });
 
-const initialGeoState = { districts };
-function geo (state = initialGeoState, action) {
+const initialGeoState = Object.assign({
+  districts
+}, getNatlCount(districts, initialNationalVote));
+
+function getNatlCount (districts, vote) {
+  const natlDemCount = districts.filter(d => d.properties.threshold > vote).length;
+  const natlRepCount = districts.filter(d => d.properties.threshold < vote).length;
+  return { natlDemCount, natlRepCount };
+}
+
+function geo (state = initialGeoState, { type, next }) {
+  switch (type) {
+    case 'SET_NATL_VOTE':
+      state = Object.assign({}, state, getNatlCount(state.districts, next.vote));
+      break;
+  }
   return state;
 }
 
 const initialVoteState = {
-  natl: 50
+  natl: initialNationalVote
 };
 
 function vote (state = initialVoteState, { type, next }) {
