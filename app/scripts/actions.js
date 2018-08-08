84a535c28aa20f8654a7cdc7934b2cde6a061e62
Derek Lieu <dl@dereklieu.com>
2018-08-08 17:34:36 -0400

    Add actions to sync app dimension and mouse state

--- app/scripts/actions.js
+++ app/scripts/actions.js
@@ -1,5 +1,13 @@
 'use strict';
 
+export function setAppDimensions ({ width, height }) {
+  return { type: 'set_app_dimensions', next: { width, height } };
+}
+
+export function syncMouse (next) {
+  return { type: 'sync_mouse', next };
+}
+
 export function setNatlVote (vote) {
-  return { type: 'SET_NATL_VOTE', next: { vote } };
+  return { type: 'set_natl_vote', next: { vote } };
 }
