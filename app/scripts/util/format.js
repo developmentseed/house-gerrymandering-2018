8507a73fe5aff3b213a2b4d32848eeb8967a77c1
Derek Lieu <dl@dereklieu.com>
2018-08-10 16:50:49 -0400

    Include formatter for district display name

--- app/scripts/util/format.js
+++ app/scripts/util/format.js
@@ -1,8 +1,21 @@
 'use strict';
+import { get } from 'object-path';
+
+const fipsToState = require('../static/fipsToState.json');
+
 export const na = '--';
 export function pct (n) {
   if (isNaN(n)) {
     return na;
   }
   return n + '%';
+}
+
+export function districtName (stateFips, districtFips) {
+  const state = get(fipsToState, stateFips);
+  if (!state) {
+    return na;
+  }
+  const district = Number(districtFips);
+  return district ? `${state} - ${district}` : state;
 }
