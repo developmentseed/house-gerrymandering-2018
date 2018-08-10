04712335da231f90a80441594096a4a9cde2baaa
Derek Lieu <dl@dereklieu.com>
2018-08-10 16:48:32 -0400

    Add ability to lock the mouse during zoom

--- app/scripts/reducers/mouse.js
+++ app/scripts/reducers/mouse.js
@@ -4,11 +4,12 @@ const initialState = {
   event: null,
   district: null,
   x: 0,
-  y: 0
+  y: 0,
+  locked: false
 };
 
 function mouse (state = initialState, { type, next }) {
-  if (type === 'sync_mouse') {
+  if (type === 'sync_mouse_location' && (!state.locked || (next.hasOwnProperty('locked') && !next.locked))) {
     state = Object.assign({}, state, next);
   }
   return state;
