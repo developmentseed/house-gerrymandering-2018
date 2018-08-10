fed156d42832478592e875639b16d4c304cfc24c
Derek Lieu <dl@dereklieu.com>
2018-08-10 16:49:08 -0400

    Set selected state geoJSON feature on click

--- app/scripts/reducers/vote.js
+++ app/scripts/reducers/vote.js
@@ -1,5 +1,6 @@
 'use strict';
-import { feature as toGeojson } from 'topojson';
+import { get } from 'object-path';
+import { feature as toGeojson, merge } from 'topojson';
 const raw = require('../static/tl_2016_us_cd115-quantized-topo.json');
 const districts = toGeojson(raw, raw.objects.districts).features;
 
@@ -10,8 +11,49 @@ districts.forEach(d => {
 
 const initialNationalVote = 50;
 const initialGeoState = Object.assign({
-  districts
+  districts,
+  focused: null,
+  selected: null
 }, getNatlCount(districts, initialNationalVote));
+
+export function geo (state = initialGeoState, { type, next }) {
+  switch (type) {
+    case 'set_natl_vote':
+      state = Object.assign({}, state, getNatlCount(state.districts, next.vote));
+      break;
+    case 'sync_mouse_location':
+      // Only set new state on a new hovered district
+      state = next.district && next.district !== get(state.focused, 'properties.id')
+        ? Object.assign({}, state, {
+          focused: findDistrict(state.districts, next.district)
+        })
+        : state;
+      break;
+    case 'sync_selected_state':
+      state = Object.assign({}, state, getSelectedState(state.districts, next.districtId));
+      break;
+  }
+  return state;
+}
+
+function findDistrict (districts, id) {
+  for (let i = 0; i < districts.length; ++i) {
+    if (districts[i].properties.id === id) {
+      return Object.assign({}, districts[i]);
+    }
+  }
+  return null;
+}
+
+function getStateDistricts (districts, id) {
+  const district = findDistrict(districts, id);
+  const { fips, stateFips } = district.properties;
+  if (Number(fips) === 0) {
+    return [district];
+  } else {
+    return districts.filter(d => d.properties.stateFips === stateFips);
+  }
+}
 
 function getNatlCount (districts, vote) {
   const natlDemCount = districts.filter(d => d.properties.threshold > vote).length;
@@ -19,13 +61,16 @@ function getNatlCount (districts, vote) {
   return { natlDemCount, natlRepCount };
 }
 
-export function geo (state = initialGeoState, { type, next }) {
-  switch (type) {
-    case 'set_natl_vote':
-      state = Object.assign({}, state, getNatlCount(state.districts, next.vote));
-      break;
+// Returns a geojson feature representing the state that the district is part of.
+function getSelectedState (districts, districtId) {
+  // a null value here will reset the map view to national
+  if (!districtId) {
+    return { selected: null };
   }
-  return state;
+  const selected = getStateDistricts(districts, districtId);
+  const idMap = new Map(selected.map(d => [d.properties.id, true]));
+  const merged = merge(raw, raw.objects.districts.geometries.filter(d => idMap.has(d.properties.id)));
+  return { selected: merged };
 }
 
 const initialVoteState = {
