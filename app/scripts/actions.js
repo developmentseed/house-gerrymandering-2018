fc87e9449e354bc5e687dcfaf20cf2bccb4119ab
Derek Lieu <dl@dereklieu.com>
2018-08-20 17:08:25 -0400

    Abstract query code, create function to query state analysis

--- app/scripts/actions.js
+++ app/scripts/actions.js
@@ -1,5 +1,5 @@
 'use strict';
-import { csv } from 'd3';
+import { csv, json } from 'd3';
 import { error } from './util/log';
 
 export function setAppDimensions ({ width, height }) {
@@ -18,22 +18,37 @@ export function setNatlVote (vote) {
   return { type: 'set_natl_vote', next: { vote } };
 }
 
-function getCsv (filename, actionName) {
+export function setStateVote (stateFips, vote) {
+  return { type: 'set_state_vote', next: { stateFips, vote } };
+}
+
+export function clearStateVote (stateFips) {
+  return { type: 'clear_state_vote', next: { stateFips } };
+}
+
+export function getHistoricalData () {
+  return getCsv('historical-results.csv', 'get_historical');
+}
+
+export function getStateThresholds () {
+  return getCsv('state-thresholds.csv', 'get_state_threshold');
+}
+
+export function getStateAnalysis () {
+  return getJson('state-analysis.json', 'get_state_analysis');
+}
+
+var getCsv = getData.bind(null, csv);
+var getJson = getData.bind(null, json);
+
+function getData (method, filename, actionName) {
   return (dispatch) => {
     dispatch({ type: actionName + '_inflight' });
-    csv(`static/${filename}.csv`).then(results => {
+    method(`static/${filename}`).then(results => {
       dispatch({ type: actionName + '_success', results });
     }).catch(e => {
       error('Loading data failed:', e.message);
       dispatch({ type: actionName + '_failed', error: e.message });
     });
   };
-}
-
-export function getHistoricalData () {
-  return getCsv('historical-results', 'get_historical');
-}
-
-export function getStateThresholds () {
-  return getCsv('state-thresholds', 'get_state_threshold');
 }
