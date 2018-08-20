180dc7b25a3dda14572cad424b6a11a81ed3afaa
Derek Lieu <dl@dereklieu.com>
2018-08-20 13:56:52 -0400

    Create facilities to standardize state IDs

--- app/scripts/util/format.js
+++ app/scripts/util/format.js
@@ -1,5 +1,6 @@
 'use strict';
 import { get } from 'object-path';
+import { error } from './log';
 
 const fipsToState = require('../static/fips-to-state.json');
 
@@ -24,6 +25,28 @@ export function districtId (stateFips, districtFips) {
   let s = +stateFips >= 10 ? stateFips : `0${+stateFips}`;
   let d = +districtFips >= 10 ? districtFips : `0${+districtFips}`;
   return `${s}${d}`;
+}
+
+export function stateId (districtId) {
+  let d = districtId.toString();
+  if (isNaN(districtId) || (d.length !== 4 && d.length !== 3)) {
+    error('Malformed district id ' + districtId);
+    return '00';
+  }
+  // Normalize district IDs lacking a leading '0'.
+  if (d.length === 3) {
+    d = '0' + d;
+  }
+  return districtId.slice(0, 2);
+}
+
+export function stateNameFromFips (stateFips) {
+  let stateName = get(fipsToState, stateFips.toString());
+  if (!stateName) {
+    error('Malformed state fips ' + stateFips);
+    return na;
+  }
+  return stateName;
 }
 
 export function party (party) {
